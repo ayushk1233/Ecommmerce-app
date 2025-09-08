6279687e305c1ba333dbbbd5f77063a6bd6a706c
ayushk1233 <ayushk1233@gmail.com>
2025-09-08 23:25:29 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
 });
 
 // --- Connect to MongoDB ---
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
